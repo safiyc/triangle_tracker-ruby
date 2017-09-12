ae6ddb04f498ad038213b5ae978b6f388cfc003a
Debbie Michel and Safiy Cham <safiy@>
2017-09-11 17:39:26 -0700

    add check for isosceles triangle.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,10 +6,19 @@ class Triangle
   end
 
   def triangle?
-    if (@side1 + @side2) > @side3
-      true
+    @side1 = @side1.to_i
+    @side2 = @side2.to_i
+    @side3 = @side3.to_i
+    if ((@side1 + @side2) > @side3) && ((@side1 + @side3) > @side2) && ((@side2 + @side3) > @side1)
+      if(@side1 == @side2 && @side2 == @side3)
+        "This is an equilateral triangle."
+
+      elsif((@side1 == @side2 && @side1 != @side3 && @side2 != @side3))
+        "This is an isosceles triangle."
+      end
     else
-      false
+      "This is not a triangle."
     end
   end
+
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,17 +2,20 @@ require('rspec')
 require('triangle')
 require 'pry'
 
-describe(Triangle) do
   describe("#triangle?") do
     it("returns false if it is not a triangle") do
       test_triangle = Triangle.new(2, 2, 8)
-      expect(test_triangle.triangle?()).to(eq(false))
+      expect(test_triangle.triangle?()).to(eq("This is not a triangle."))
     end
 
-    it("returns true if it is a triangle") do
+    it("returns equilateral if all sides equal") do
       test_triangle = Triangle.new(2, 2, 2)
-      expect(test_triangle.triangle?()).to(eq(true))
+      expect(test_triangle.triangle?()).to(eq("This is an equilateral triangle."))
+    end
+
+    it("returns isosceles if exactly 2 sides are equal") do
+      test_triangle = Triangle.new(2, 2, 1)
+      expect(test_triangle.triangle?()).to(eq("This is an isosceles triangle."))
     end
 
   end
-end
